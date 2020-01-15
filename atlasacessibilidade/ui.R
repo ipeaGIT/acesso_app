4b7ebd3e0d768bc40842340a48c14b35cd2f82bf
kauebraga <kauebraga90@gmail.com>
2020-01-15 05:59:47 +0000

    add popup tips and crousels

--- atlasacessibilidade/ui.R
+++ atlasacessibilidade/ui.R
@@ -79,13 +79,21 @@ div(class = "navbar-default"
                                                    # label = HTML("<h1>Escolha o indicador de acessibilidade: <img src=\"ipea.jpg\" align=\"leftright\" width=\"70\"/></h1>"),
                                                    label = HTML("<h1>Escolha o indicador de acessibilidade: 
                                                                   <button id=\"q1\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>"),
-                                                   choices = c("Cumulativo" ="CMA", "Oportunidade mais próxima" = "TMI"),
+                                                   choices = c("Cumulativo" ="CMA", "Tempo Mínimo" = "TMI"),
                                                    selected = "CMA"),
+                                      div(
+                                        # edit2
+                                        bsPopover(id = "q1", 
+                                                  title = "<strong>Indicadores de acessibilidade</strong>",
+                                                  content = HTML("<ul><li><strong>Indicador cumulativo</strong> representa a proporção de oportunidades em relação ao total da cidade que podem ser alcançadas dado um tempo máximo de viagem</li><li><strong>Tempo mínimo</strong> é o tempo de viagem até a oportunidade mais próxima</li></ul>"),
+                                                  placement = "bottom",
+                                                  trigger = "hover",
+                                                  options = list(container = "body"))
+                                      ),
                                       conditionalPanel(condition = "cities_todos.indexOf(input.cidade) > -1", 
                                                        radioGroupButtons(inputId = "modo_todos",
                                                                          # label = HTML("<h1>Escolha o indicador de acessibilidade: <img src=\"ipea.jpg\" align=\"leftright\" width=\"70\"/></h1>"),
-                                                                         label = HTML("<h1>Escolha o modo de transporte: 
-                                                                  <button id=\"q2\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>"),
+                                                                         label = HTML("<h1>Escolha o modo de transporte:"), 
                                                                          choices = c("<i class=\"fas fa-bus fa-2x\"></i>" = "tp", 
                                                                                      "<i class=\"fas fa-walking fa-2x\"></i>" = "caminhada",
                                                                                      "<i class=\"fas fa-bicycle fa-2x\"></i>" = "bicicleta"),
@@ -96,8 +104,7 @@ div(class = "navbar-default"
                                       conditionalPanel(condition = "cities_ativo.indexOf(input.cidade) > -1", 
                                                        radioGroupButtons(inputId = "modo_ativo",
                                                                          # label = HTML("<h1>Escolha o indicador de acessibilidade: <img src=\"ipea.jpg\" align=\"leftright\" width=\"70\"/></h1>"),
-                                                                         label = HTML("<h1>Escolha o modo de transporte: 
-                                                                  <button id=\"q2\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>"),
+                                                                         label = HTML("<h1>Escolha o modo de transporte:</h1>"),
                                                                          choices = c("<i class=\"fas fa-walking fa-2x\"></i>" = "caminhada",
                                                                                      "<i class=\"fas fa-bicycle fa-2x\"></i>" = "bicicleta"),
                                                                          # choiceNames = list(HTML("<i class=\"fas fa-walking fa-2x\"></i>"),
@@ -113,19 +120,10 @@ div(class = "navbar-default"
                                                                          #   no = tags$i(class = "fa fa-circle-o", 
                                                                          #               style = "color: steelblue"))
                                                        )),
-                                      div(
-                                        # edit2
-                                        bsPopover(id = "q1", 
-                                                  title = "Indicadores de acessibilidade",
-                                                  content = HTML("<strong>Indicador cumulativo</strong> representa a proporção de oportunidades em relação ao total da cidade que podem ser alcançadas dado um tempo máximo de viagem<br>Tempo mínimo ...."),
-                                                  placement = "top",
-                                                  trigger = "hover",
-                                                  options = list(container = "body"))
-                                      ),
                                       # img(src='ipea.jpg', align = "right", width = "150"),
                                       conditionalPanel(condition = "input.indicador == 'CMA'",
                                                        pickerInput(inputId = "atividade_cum",
-                                                                   label = h1("Escolha a atividade:"),
+                                                                   label = HTML("<h1>Escolha a atividade: <button id=\"q3\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>"),
                                                                    choices = list(
                                                                      'Trabalho' = c("Trabalho Total" = "TT"
                                                                                     # , "Trabalho Quintil" = "TQ"
@@ -142,8 +140,8 @@ div(class = "navbar-default"
                                                                    selected = "Trabalho Total")),
                                       conditionalPanel(condition = "input.indicador == 'TMI'",
                                                        selectInput(inputId = "atividade_min",
-                                                                   label = h1("Escolha a atividade:"),
-                                                                   choices = list(
+                                                                   label = HTML("<h1>Escolha a atividade: <button id=\"q4\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>"),
+                                                                       choices = list(
                                                                      'Saúde' = c("Saúde Total" = "ST",
                                                                                  "Saúde Baixa" = "SB",
                                                                                  "Saúde Média" = "SM",
@@ -153,6 +151,24 @@ div(class = "navbar-default"
                                                                                     "Educação Fundamental" = "EF",
                                                                                     "Educação Média" = "EM")),
                                                                    selected = "Saúde")),
+                                      div(
+                                        # edit2
+                                        bsPopover(id = "q3", 
+                                                  title = HTML("<strong>Atividades</strong>"),
+                                                  content = HTML("<ul><li> Atividades com o sufixo <em>Total</em> representam a soma das subdivisões da atividade</li><li> Sufixos da atividade de <b>saúde</b> (<em>Baixa, Média</em> e <em>Alta</em>) representam a complexidade daqueles estabelecimentos</li></ul>"),
+                                                  placement = "top",
+                                                  trigger = "hover",
+                                                  options = list(container = "body"))
+                                      ),
+                                      div(
+                                        # edit2
+                                        bsPopover(id = "q4", 
+                                                  title = HTML("<strong>Atividades</strong>"),
+                                                  content = HTML("<ul><li> Atividades com o sufixo <em>Total</em> representam a soma das subdivisões da atividade</li><li> Sufixos da atividade de <b>saúde</b> (<em>Baixa, Média</em> e <em>Alta</em>) representam a complexidade daqueles estabelecimentos</li></ul>"),
+                                                  placement = "top",
+                                                  trigger = "hover",
+                                                  options = list(container = "body"))
+                                      ),
                                       conditionalPanel(condition = "cities_todos.indexOf(input.cidade) > -1 && input.indicador == 'CMA' && input.modo_todos == 'tp'",
                                                        sliderInput(inputId = "tempo_tp",
                                                                    label = h1("Escolha o tempo de viagem:"),
