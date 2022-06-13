3ec0082aa784bc45cb5f9a3922e48c94b0366901
Kaue Braga <kauebraga90@gmail.com>
2022-06-13 14:23:04 -0300

    remove h1 from label info

--- atlasacessibilidade/R/label_with_info.R
+++ atlasacessibilidade/R/label_with_info.R
@@ -1,6 +1,6 @@
 label_with_info <- function(label, tooltip_id) {
   
-  x <- HTML(sprintf("<h1>%s <button id=\"%s\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button></h1>", 
+  x <- HTML(sprintf("%s <button id=\"%s\" type=\"button\" class=\"btn btn-light btn-xs\"><i class=\"fa fa-info\"></i></button>", 
                     label, tooltip_id))
   
   return(x)
